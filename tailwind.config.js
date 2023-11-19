8d9d37373225f664aa728662a182f16141fa0aa9
Sajjad Anghooty <anghootys@gmail.com>
2023-11-20 02:40:50 +0330

    config: tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,16 @@ module.exports = {
     "./src/**/*.{html,ts}"
   ],
   theme: {
+    colors: {
+      'blue': "#0B5EA1",
+      'gray': "#3F4B4D",
+      'green-0': "#1B9E2B",
+      'green-1': "#1B9F85",
+      'green-2': "#1B9E57",
+    },
+    fontFamily: {
+      'vazir': ['Vazirmatn', 'sans-serif']
+    },
     extend: {},
   },
   plugins: [],
