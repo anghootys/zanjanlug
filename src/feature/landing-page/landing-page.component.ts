f90a674e64aecc9823d4ef782156281fd07fde12
Sajjad Anghooty <anghootys@gmail.com>
2023-11-25 00:11:20 +0330

    create: landing-page component

---
+++ src/feature/landing-page/landing-page.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {RouterLink} from "@angular/router";
+
+@Component({
+  selector: 'app-landing-page',
+  standalone: true,
+  imports: [CommonModule, RouterLink],
+  templateUrl: './landing-page.component.html',
+  styleUrl: './landing-page.component.scss'
+})
+export class LandingPageComponent {
+
+}
